a3cf64fe5458ecf1431967f53c6b1651132ce5a7
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-04-02 19:16:22 +0300

    chore(common/network): `PACKET_SIZE` is changed

--- apps/common/src/main/java/s1riys/lab6/common/constants/Network.java
+++ apps/common/src/main/java/s1riys/lab6/common/constants/Network.java
@@ -2,7 +2,7 @@ package s1riys.lab6.common.constants;
 
 public interface Network {
     Integer PORT = 21337;
-    Integer PACKET_SIZE = 1024;
+    Integer PACKET_SIZE = 128;
     Integer DATA_SIZE = PACKET_SIZE - 1;
     byte STOP_BYTE = (byte) '>';
     byte CONTINUE_BYTE = (byte) '#';
